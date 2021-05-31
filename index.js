3ff84b5ae7673f7610dfece2219846441c17e215
Fury-101 <73406313+Fury-101@users.noreply.github.com>
2021-05-31 08:17:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -483,7 +483,7 @@
         if(!guild_unmute_rank_staff){var guild_unmute_rank_staff = ''}
         if(!guild_unmute_rank_username){var guild_unmute_rank_username = ''}
         client.channels.cache.get(staffChannel).send(`-----------------------------------------------------\n**${guild_unmute_rank_staff} ${guild_unmute_staff}** has unmuted **${guild_unmute_rank_username} ${guild_unmute_username}**\n-----------------------------------------------------`)
-        let displayNickname = guild_mute_username;
+        let displayNickname = guild_unmute_username;
         let serverMembers = client.guilds.cache.get(serverID).members.cache;
         let matchedMember = serverMembers.findKey(user => user.nickname == displayNickname);
         if (!matchedMember) {return}
