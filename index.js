a8029f9865a6cfaa91b47c5021b6e41c0bcc3315
Fury-101 <73406313+Fury-101@users.noreply.github.com>
2021-06-16 12:14:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@
       logger.info(`I could not find the channel (${channelID})! -- 2`)
       process.exit(1)
     }
-    fetch("https://api.hypixel.net/playercount?key=HypixelAPIKey")
+    fetch(`https://api.hypixel.net/playercount?key=${HypixelAPIKey}`)
     .then(response => response.json())
     .then(data => {
       client.user.setPresence({ activity: { name: data.playerCount.toString()+" players" , type:"WATCHING" }, status: 'online' });
