4cd0c3c362280ec69856293635ad8417fa9c2420
Fury-101 <73406313+Fury-101@users.noreply.github.com>
2021-07-21 17:48:09 -0700

    make disc -> guild work with rowdies's name changing fetish

--- index.js
+++ index.js
@@ -658,7 +658,7 @@
     if(message.attachments.size > 0){return}
     var user = message.guild.member(message.member)
     if(message.content.length > 100){return message.channel.send(`Your message is too long! ${message.content.length}/100`)}
-    bot.chat(`/gc [${user.displayName}] - ${message.content}`)
+    bot.chat(`/gc [${user.displayName.split(" ")[0]}] - ${message.content}`)
     McChatLogger.info(`DISCORD > [${message.author.tag}/${message.author.id}]: ${message.content}`)
     message.delete()
   }
