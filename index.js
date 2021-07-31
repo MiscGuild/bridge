b22db7c441f759c61338f6c6e54d62ca58f4d8b3
Jeje <69335633+Jejebecarte@users.noreply.github.com>
2021-07-31 14:49:33 +1000

    Guild Request Handling
    The bot now handles all guild join requests, disregarding blacklisted players, and players that are under network level 50.
    Players under lvl 50 can still be manually let in.

--- index.js
+++ index.js
@@ -162,7 +162,9 @@
   var colour = [];
   var sLogs = []
 
-
+  function getNetworkLevel(exp){
+    return (Math.sqrt((2 * exp) + 30625) / 50) - 2.5
+  }
 
   bot.once('spawn', () => {
 
@@ -444,7 +446,26 @@
       const guild_requesting = (guild_requesting_rank, guild_requesting_username) => {
         if(!guild_requesting_rank){var guild_requesting_rank = ''}
         // logger.info(`-----------------------------------------------------\n**${guild_requesting_rank} ${guild_requesting_username}** is requesting to join the guild! \nA staff member can do \`)command g accept ${guild_requesting_username}\`\n-----------------------------------------------------`)
-        messages.push(`-----------------------------------------------------\n**${guild_requesting_rank} ${guild_requesting_username}** is requesting to join the guild! \nA staff member can do \`)command g accept ${guild_requesting_username}\`\n-----------------------------------------------------`)
+        async function check_requesting_user(){
+          const MojangAPI = await fetch(`https://api.ashcon.app/mojang/v2/user/${guild_requesting_username}`)
+        .then(res => res.json())
+        for(var i in blacklist){
+          var guild_requesting_uuid = MojangAPI.uuid
+          if(blacklist[i].uuid == guild_requesting_uuid){
+          return bot.chat(`/oc The player ${guild_requesting_username} is on the blacklist! Do **NOT** accept their request.`)
+          }
+        }
+        
+        const HyAPI = await fetch(`https://api.hypixel.net/player?key=${process.env.HypixelAPIKey}&uuid=${guild_requesting_uuid}&player=${guild_requesting_username}`)
+        .then(response => response.json())
+        if (getNetworkLevel(HyAPI.player.networkExp) > 50) {
+          bot.chat(`/g accept ${guild_requesting_username}`)
+        }
+        else {
+          bot.chat(`/oc The player ${guild_requesting_username} is not network level 50!`)
+        }
+        }
+        check_requesting_user()
       }
 
       bot.chatAddPattern(
