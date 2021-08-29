71824157382e23750a67255150dec00f5489f41e
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-08-29 18:13:56 +1000

    Move events
    Oopsy didn't do it last commit

---
+++ events/discord/staffMessage.js
@@ -0,0 +1,33 @@
+const index = require("../../index.js");
+const log4js =  require('log4js');
+const bot = index.bot;
+const staffChannelID = index.staffChannelID;
+const McChatLogger = log4js.getLogger("McChatLogs");
+
+module.exports = {
+  name: "message",
+  async execute(message) {
+    if (message.channel.id == staffChannelID) {
+      if (message.content.startsWith(process.env.PREFIX)) {
+        return;
+      }
+      if (message.author.bot) {
+        return;
+      }
+      if (message.attachments.size > 0) {
+        return;
+      }
+      var user = message.guild.member(message.member);
+      if (message.content.length > 250) {
+        return message.channel.send(
+          `Your message is too long! ${message.content.length}/250`
+        );
+      }
+      bot.chat(`/oc [${user.displayName.split(" ")[0]}] -  ${message.content}`);
+      McChatLogger.info(
+        `DISCORD (OFFICER CHAT)> [${message.author.tag}/${message.author.id}]: ${message.content}`
+      );
+      message.delete();
+    }
+  },
+};
---
+++ events/minecraft/login.js
@@ -0,0 +1,27 @@
+const index = require('../../index.js');
+const cron = require('node-cron');
+const log4js =  require('log4js');
+const client = index.client;
+const bot = index.bot;
+const channel = client.channels.cache.get(index.channelID);
+const logger = log4js.getLogger("Logs");
+
+module.exports = {
+    name: 'login',
+    async execute(){
+        logger.info('The bot has logged in!');
+
+        cron.schedule('0 * * * *', () => {
+            channel.send(`I will AUTO Reboot in ONE minute. I will be back in 30 seconds!`);
+        });
+
+        setInterval(function(){
+            bot.chat('/chat g');
+            bot.chat('/ac \u00a7');
+        }, 3000000);
+
+        bot.chat('/ac \u00a7')
+        bot.chat('/chat g');
+        bot.chat('/g online');
+    }
+}
