74c3c341ea389ceaeabf8acb2f78ae17cb846345
Luckyy <64851720+luckyywastaken@users.noreply.github.com>
2021-08-31 14:58:22 -0400

    Create auto updating help command

---
+++ SlashCommands/help.js
@@ -0,0 +1,35 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { glob } = require("glob");
+const { promisify } = require("util");
+
+const globPromise = promisify(glob);
+
+module.exports = {
+    name: "help",
+    description: "Sends all the commands in the bot!",
+    type: 'CHAT_INPUT',
+ 
+    run: async (client, interaction, args) => {
+        const slashCommands = await globPromise(
+            `${process.cwd()}/SlashCommands/*.js`
+        );
+    
+        const arrayOfSlashCommands = [];
+        slashCommands.map((value) => {
+            const file = require(value);
+            if (!file?.name) return;
+            client.slashCommands.set(file.name, file);
+    
+            if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+            arrayOfSlashCommands.push(file);
+        });
+    
+
+        const embed = new MessageEmbed()
+            .setTitle('Commands')
+            arrayOfSlashCommands.forEach(i => embed.addField(i.name, i.description)) 
+        interaction.followUp({ embeds: [embed] })
+        
+
+    },
+};
