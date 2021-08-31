c48638e2f359e48a2a60fb5e655905ef1635019c
Luckyy <64851720+luckyywastaken@users.noreply.github.com>
2021-08-31 14:32:04 -0400

    Made reboot into a slash command

---
+++ SlashCommands/reboot.js
@@ -0,0 +1,44 @@
+const { Client, CommandInteraction } = require("discord.js");
+const Discord = require("discord.js");
+const log4js = require("log4js");
+const logger = log4js.getLogger("logs");
+
+module.exports = {
+    name: "reboot",
+    description: "Reboots the bot",
+    type: 'CHAT_INPUT',
+ 
+    run: async (client, interaction, args) => {
+        let channel = client.channels.cache.get(process.env.OUTPUTCHANNEL);
+
+        if (
+            interaction.member.roles.cache.some((role) => role.name === "Staff") ||
+            interaction.author.id === "308343641598984203"
+          ) {
+            const embed = new Discord.MessageEmbed()
+              .setTitle("Rebooting")
+              .setDescription("The bot will reboot in `45s`");
+    
+              interaction.followUp({ embeds: [embed] });
+    
+            logger.info(
+              `Bot will reboot in 45s due to ${interaction.member.displayName} running the reboot command`
+            );
+            channel.send(
+              `Bot will reboot in 45s due to ${interaction.member.displayName} running the reboot command`
+            );
+            setTimeout(() => {
+              process.exit();
+            }, 45000);
+          } else {
+            const embed = new Discord.MessageEmbed()
+              .setTitle("Error")
+              // .setColor(CLR)
+              .setDescription(
+                "It seems you are lacking the permission to run this command."
+              );
+    
+            return interaction.followUp({ embeds: [embed] });
+          }
+    },
+};
