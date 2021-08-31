1e7b68aa5443909b3054eb0cbfd3cd6916e501f2
Luckyy <64851720+luckyywastaken@users.noreply.github.com>
2021-08-31 16:12:02 -0400

    Bold left the game message

--- eventfunctions/guildLeftGame.js
+++ eventfunctions/guildLeftGame.js
@@ -5,6 +5,6 @@ module.exports = {
     name: "guild_left_game",
     async execute(guild_left_game_name){
         // logger.info(`${guild_left_game_name} left the game.`);
-        sendToDiscord(`${guild_left_game_name} left the game.`);
+        sendToDiscord(`**${guild_left_game_name}** left the game.`);
     }   
 }
