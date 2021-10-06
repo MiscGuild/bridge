647509fe5faa7c6390d9137c52b5999bd651a125
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-10-06 14:03:06 +1100

    Add custom Discord emoji support

--- events/discord/message.js
+++ events/discord/message.js
@@ -7,21 +7,24 @@ export default {
 	name: "message",
 	async execute(message) {
 		if (message.channel.id == channelID) {
-			if (message.content.startsWith(prefix)) {
+			if (message.content.startsWith(prefix) || message.author.bot || message.attachments.size > 0) {
 				return;
 			}
-			if (message.author.bot) {
-				return;
+
+			const customEmojis = message.content.match(/(?!:)\w*(?=:)/g); // Grab name of custom Discord emojis <:NAME:ID>
+			if (customEmojis) {
+				for (const emojiName of customEmojis) {
+					message.content = message.content.replace(/<a:.+?:\d+>|<:.+?:\d+>/, ":" + emojiName + ":"); // Replace the first occuring custom Discord emoji
+				}
 			}
-			if (message.attachments.size > 0) {
-				return;
-			}
-			const user = message.member;
+
 			if (message.content.length > 100) {
 				return message.channel.send(
 					`Your message is too long! ${message.content.length}/100`
 				);
 			}
+
+			const user = message.member;
 			bot.chat(`/gc [${user.displayName.split(" ")[0]}] - ${message.content}`);
 			McChatLogger.info(
 				`DISCORD > [${message.author.tag}/${message.author.id}]: ${message.content}`
