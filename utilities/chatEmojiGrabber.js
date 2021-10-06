f832136743a6794d67fbd6945a15baf6e41c662e
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2021-10-06 14:59:42 +1100

    Fix chatEmojiGrabber for nons

--- utilities/chatEmojiGrabber.js
+++ utilities/chatEmojiGrabber.js
@@ -14,15 +14,19 @@ export async function getRankEmoji(rank) {
 		await setEmojis();
 	}
 
-	const rankData = {
-		"": { emojis: "", color: "0xAAAAAA" },
-		"[VIP]": { emojis: `\u200D  ${VIP1}${VIP2}${VIP3}`, color: "0x55FF55" },
-		"[VIP+]": { emojis: `\u200D     ${VIPPLUS1}${VIPPLUS2}${VIPPLUS3}`, color: "0x55FF55" },
-		"[MVP]": { emojis: `\u200D   ${MVP1}${MVP2}${MVP3}`, color: "0x55FFFF" },
-		"[MVP+]": { emojis: `${MVPPLUS1}${MVPPLUS2}${MVPPLUS3}${MVPPLUS4}`, color: "0x55FFFF" },
-		"[MVP++]": { emojis: `\u200D    ${MVPPLUSPLUS1}${MVPPLUSPLUS2}${MVPPLUSPLUS3}${MVPPLUSPLUS4}`, color: "0xFFAA00" },
-	};
-	return [rankData[rank].emojis, rankData[rank].color];
+	if (!rank) {
+		return ["", "0xAAAAAA"];
+	}
+	else {
+		const rankData = {
+			"[VIP]": { emojis: `\u200D  ${VIP1}${VIP2}${VIP3}`, color: "0x55FF55" },
+			"[VIP+]": { emojis: `\u200D     ${VIPPLUS1}${VIPPLUS2}${VIPPLUS3}`, color: "0x55FF55" },
+			"[MVP]": { emojis: `\u200D   ${MVP1}${MVP2}${MVP3}`, color: "0x55FFFF" },
+			"[MVP+]": { emojis: `${MVPPLUS1}${MVPPLUS2}${MVPPLUS3}${MVPPLUS4}`, color: "0x55FFFF" },
+			"[MVP++]": { emojis: `\u200D    ${MVPPLUSPLUS1}${MVPPLUSPLUS2}${MVPPLUSPLUS3}${MVPPLUSPLUS4}`, color: "0xFFAA00" },
+		};
+		return [rankData[rank].emojis, rankData[rank].color];
+	}
 }
 
 export async function getTagEmoji(tag) {
