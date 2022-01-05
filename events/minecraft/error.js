c527b66ddba7c54de81709ada5fdc785a43848c2
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-01-05 18:18:46 +1100

    fix(master): prevent undefined channel on Mineflayer error

--- events/minecraft/error.js
+++ events/minecraft/error.js
@@ -2,7 +2,6 @@ import { client } from "../../index.js";
 import { channelID } from "../../resources/consts.js";
 import log4js from "log4js";
 const errorLogs = log4js.getLogger("Errors");
-const channel = client.channels.cache.get(channelID);
 
 export default {
 	name: "error",
@@ -10,7 +9,7 @@ export default {
 	execute(err) {
 		console.log("Error attempting to reconnect: " + err + ".");
 		errorLogs.error("Error attempting to reconnect: " + err + ".");
-		channel.send("ERROR: An unexpected error has occurred. Rebooting in 30s...");
+		client.channels.cache.get(channelID).send("ERROR: An unexpected error has occurred. Rebooting in 30s...");
 
 		setTimeout(function() {
 			console.log("Shutting down for automatic relog");
