0b9390b7f5b8e8e536dc06e2d21eaaf24d0ce858
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-24 13:26:47 +1000

    feat: add gexp checking to whisper event

--- src/events/mineflayer/chat/whisper.ts
+++ src/events/mineflayer/chat/whisper.ts
@@ -1,9 +1,29 @@
 import { Event } from "../../../interfaces/Event";
+import fetchHypixelPlayerProfile from "../../../util/fetchHypixelPlayerProfile";
+import fetchMojangProfile from "../../../util/fetchMojangProfile";
+import isFetchError from "../../../util/isFetchError";
 
 export default {
 	name: "chat:whisper",
 	runOnce: false,
 	run: async (bot, playerName: string, message: string) => {
-		// TODO: Gexp checking?, event registration
+		const target = message.startsWith("weeklygexp" || "weeklygxp") ? playerName : (message.split(" ")[0] as string);
+		const mojangProfile = await fetchMojangProfile(target);
+		const hypixelPlayerProfile = await fetchHypixelPlayerProfile(target);
+
+		if (isFetchError(mojangProfile) || isFetchError(hypixelPlayerProfile)) {
+			bot.executeCommand(
+				`/w ${playerName} There was an error attempting your request! (Check spelling and/or try again later)`,
+			);
+		} else {
+			const member = hypixelPlayerProfile.members.find((guildMember) => guildMember.uuid === mojangProfile.id);
+
+			bot.executeCommand(
+				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+				`/w ${playerName} ${target}'s total weekly gexp: ${Object.values(member!.expHistory).reduce(
+					(previous, current) => previous + current,
+				)}`,
+			);
+		}
 	},
 } as Event;
---
+++ src/util/fetchHypixelPlayerProfile.ts
@@ -0,0 +1,9 @@
+import { FetchError } from "../interfaces/FetchError";
+import { HypixelGuildResponse } from "../interfaces/HypixelGuildReponse";
+import fetch from "node-fetch";
+
+export default async (uuid: string) => {
+	const response = await fetch(`https://api.hypixel.net/guild?key=${process.env.HYPIXEL_API_KEY}&player=${uuid}`);
+
+	return response.status === 200 ? ((await response.json()) as HypixelGuildResponse) : (response as FetchError);
+};
