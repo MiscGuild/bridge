bd40b9706e352411bdb71165f05b1ace88080d51
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-24 20:52:57 +1000

    feat: add network level checking to joinRequest

--- src/events/mineflayer/chat/joinRequest.ts
+++ src/events/mineflayer/chat/joinRequest.ts
@@ -1,17 +1,31 @@
 import { BlacklistEntry } from "../../../interfaces/BlacklistEntry";
 import { Event } from "../../../interfaces/Event";
+import _blacklist from "../../../util/_blacklist.json";
+import fetchHypixelPlayerProfile from "../../../util/fetchHypixelPlayerProfile";
 import fetchMojangProfile from "../../../util/fetchMojangProfile";
 import isFetchError from "../../../util/isFetchError";
-import _blacklist from "../../../util/_blacklist.json";
 
 export default {
 	name: "chat:joinRequest",
 	runOnce: false,
-	run: async (bot, playerName) => {
+	run: async (bot, playerName: string) => {
 		const blacklist = _blacklist as BlacklistEntry[];
 		const mojangProfile = await fetchMojangProfile(playerName);
 
 		if (!isFetchError(mojangProfile)) {
+			const playerProfile = await fetchHypixelPlayerProfile(playerName);
+
+			if (!isFetchError(playerProfile)) {
+				const networkLevel = Math.sqrt(2 * playerProfile.networkExp! + 30625) / 50 - 2.5;
+
+				if (networkLevel < parseFloat(process.env.MINIMUM_NETWORK_LEVEL as string)) {
+					await bot.sendGuildMessage(
+						"oc",
+						`The player ${playerName} is not network level ${process.env.MINIMUM_NETWORK_LEVEL}!`,
+					);
+				}
+			}
+
 			if (blacklist.some((entry) => entry.uuid === mojangProfile.id)) {
 				await bot.sendGuildMessage(
 					"oc",
@@ -19,7 +33,5 @@ export default {
 				);
 			}
 		}
-
-		// TODO: Do not accept network levels? (set in .env)
 	},
 } as Event;
---
+++ src/util/fetchHypixelPlayerProfile.ts
@@ -0,0 +1,13 @@
+import { FetchError } from "../interfaces/FetchError";
+import { HypixelPlayerResponse } from "../interfaces/HypixelPlayerResponse";
+import fetch from "node-fetch";
+
+export default async (username: string) => {
+	const response = await fetch(
+		`https://api.hypixel.net/player?key=${process.env.HYPIXEL_API_KEY}&name=${username}`,
+	);
+
+	return response.status === 200
+		? ((await response.json()).player as HypixelPlayerResponse)
+		: (response as FetchError);
+};
