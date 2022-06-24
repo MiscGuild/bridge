58dcf5023aaf7223a11d41e91f0aff53e610e4f5
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-24 16:59:26 +1000

    fix: make fetchHypixelGuild return guild object

--- src/events/mineflayer/chat/whisper.ts
+++ src/events/mineflayer/chat/whisper.ts
@@ -1,5 +1,5 @@
 import { Event } from "../../../interfaces/Event";
-import fetchHypixelPlayerProfile from "../../../util/fetchHypixelPlayerProfile";
+import fetchHypixelGuild from "../../../util/fetchHypixelGuild";
 import fetchMojangProfile from "../../../util/fetchMojangProfile";
 import isFetchError from "../../../util/isFetchError";
 
@@ -7,23 +7,25 @@ export default {
 	name: "chat:whisper",
 	runOnce: false,
 	run: async (bot, playerName: string, message: string) => {
+		const errorMessage = `/w ${playerName} There was an error attempting your request! (Check spelling and/or try again later)`;
 		const target = message.startsWith("weeklygexp" || "weeklygxp") ? playerName : (message.split(" ")[0] as string);
 		const mojangProfile = await fetchMojangProfile(target);
-		const hypixelPlayerProfile = await fetchHypixelPlayerProfile(target);
 
-		if (isFetchError(mojangProfile) || isFetchError(hypixelPlayerProfile)) {
-			bot.executeCommand(
-				`/w ${playerName} There was an error attempting your request! (Check spelling and/or try again later)`,
-			);
-		} else {
-			const member = hypixelPlayerProfile.members.find((guildMember) => guildMember.uuid === mojangProfile.id);
-
-			bot.executeCommand(
-				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-				`/w ${playerName} ${target}'s total weekly gexp: ${Object.values(member!.expHistory).reduce(
-					(previous, current) => previous + current,
-				)}`,
-			);
+		if (isFetchError(mojangProfile)) {
+			return bot.executeCommand(errorMessage);
 		}
+
+		const playerGuild = await fetchHypixelGuild(mojangProfile.id);
+		if (isFetchError(playerGuild)) {
+			return bot.executeCommand(errorMessage);
+		}
+
+		const member = playerGuild.members.find((guildMember) => guildMember.uuid === mojangProfile.id);
+		bot.executeCommand(
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			`/w ${playerName} ${target}'s total weekly gexp: ${Object.values(member!.expHistory).reduce(
+				(previous, current) => previous + current,
+			)}`,
+		);
 	},
 } as Event;
--- src/util/fetchHypixelGuild.ts
+++ src/util/fetchHypixelGuild.ts
@@ -5,5 +5,5 @@ import fetch from "node-fetch";
 export default async (uuid: string) => {
 	const response = await fetch(`https://api.hypixel.net/guild?key=${process.env.HYPIXEL_API_KEY}&player=${uuid}`);
 
-	return response.status === 200 ? ((await response.json()) as HypixelGuildResponse) : (response as FetchError);
+	return response.status === 200 ? ((await response.json()).guild as HypixelGuildResponse) : (response as FetchError);
 };
