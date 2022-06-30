f2628ce9bc8c3367242e6e680dcfcf1620ceb7e0
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-30 17:37:43 +1000

    feat: add mvp++ emoji images

--- src/util/emojis/images/index.ts
+++ src/util/emojis/images/index.ts
@@ -13,3 +13,7 @@ export { default as mvp3 } from "./mvp/mvp3.png";
 export { default as mvpPlus1 } from "./mvp+/mvpPlus1.png";
 export { default as mvpPlus2 } from "./mvp+/mvpPlus2.png";
 export { default as mvpPlus3 } from "./mvp+/mvpPlus3.png";
+
+export { default as mvpPlusPlus1 } from "./mvp++/mvpPlusPlus1.png";
+export { default as mvpPlusPlus2 } from "./mvp++/mvpPlusPlus2.png";
+export { default as mvpPlusPlus3 } from "./mvp++/mvpPlusPlus3.png";
