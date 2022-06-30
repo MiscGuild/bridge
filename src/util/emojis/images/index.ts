b9ca402ae540671e4819a13f37cd72c14805b2db
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-30 11:34:54 +1000

    feat: add images module types

---
+++ src/util/emojis/images/index.ts
@@ -0,0 +1,7 @@
+export { default as vip1 } from "./vip/vip1.png";
+export { default as vip2 } from "./vip/vip2.png";
+export { default as vip3 } from "./vip/vip3.png";
+
+export { default as vipPlus1 } from "./vip+/vipPlus1.png";
+export { default as vipPlus2 } from "./vip+/vipPlus2.png";
+export { default as vipPlus3 } from "./vip+/vipPlus3.png";
