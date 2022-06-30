a005bb53cb94c1c4c945df4b1122066a8c59a54b
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-06-30 17:43:27 +1000

    fix: add 4th image export for MVP+ and ++

--- src/util/emojis/images/index.ts
+++ src/util/emojis/images/index.ts
@@ -13,7 +13,9 @@ export { default as mvp3 } from "./mvp/mvp3.png";
 export { default as mvpPlus1 } from "./mvp+/mvpPlus1.png";
 export { default as mvpPlus2 } from "./mvp+/mvpPlus2.png";
 export { default as mvpPlus3 } from "./mvp+/mvpPlus3.png";
+export { default as mvpPlus4 } from "./mvp+/mvpPlus4.png";
 
 export { default as mvpPlusPlus1 } from "./mvp++/mvpPlusPlus1.png";
 export { default as mvpPlusPlus2 } from "./mvp++/mvpPlusPlus2.png";
 export { default as mvpPlusPlus3 } from "./mvp++/mvpPlusPlus3.png";
+export { default as mvpPlusPlus4 } from "./mvp++/mvpPlusPlus4.png";
