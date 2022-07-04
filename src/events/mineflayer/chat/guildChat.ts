c7364e9258451e48b335049dd56479dd44cb7a89
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-04 19:27:52 +1000

    feat: implement getHypixelRankEmojis

--- src/events/mineflayer/chat/guildChat.ts
+++ src/events/mineflayer/chat/guildChat.ts
@@ -1,6 +1,8 @@
 import Emojis from "../../../util/emojis";
 import { Event } from "../../../interfaces/Event";
+import { HypixelRank } from "../../../interfaces/Ranks";
 import { Util } from "discord.js";
+import getHypixelRankEmoji from "../../../util/emojis/getHypixelRankEmojis";
 
 export default {
 	name: "chat:guildChat",
@@ -8,12 +10,12 @@ export default {
 	run: async (
 		bot,
 		channel: "Guild" | "Officer",
-		hypixelRank: string | null,
+		hypixelRank: HypixelRank | null,
 		playerName: string,
 		guildRank: string | null,
 		message: string,
 	) => {
-		const formattedMessage = ` **${hypixelRank ?? ""}${Util.escapeMarkdown(playerName)}${
+		const formattedMessage = `**${await getHypixelRankEmoji(hypixelRank)}${Util.escapeMarkdown(playerName)}${
 			" " + guildRank ?? ""
 		}:** ${Util.escapeMarkdown(message)}`;
 		channel === "Guild"
--- src/util/emojis/getHypixelRankEmojis.ts
+++ src/util/emojis/getHypixelRankEmojis.ts
@@ -2,28 +2,30 @@ import { HypixelRank, VerboseHypixelRank } from "../../interfaces/Ranks";
 import { EmojiIds } from "../../interfaces/EmojiIds";
 import _emojiIds from "./_emojiIds.json";
 
-export default async (rank: HypixelRank) => {
+export default async (rank: HypixelRank | null) => {
 	const emojiIds = _emojiIds as EmojiIds;
 	let rankName: VerboseHypixelRank;
 
 	switch (rank) {
-	case "[VIP]":
-		rankName = "vip";
-		break;
-	case "[VIP+]":
-		rankName = "vipPlus";
-		break;
-	case "[MVP]":
-		rankName = "mvp";
-		break;
-	case "[MVP+]":
-		rankName = "mvpPlus";
-		break;
-	case "[MVP++]":
-		rankName = "mvpPlusPlus";
-		break;
-	default:
-		return rank;
+		case "[VIP]":
+			rankName = "vip";
+			break;
+		case "[VIP+]":
+			rankName = "vipPlus";
+			break;
+		case "[MVP]":
+			rankName = "mvp";
+			break;
+		case "[MVP+]":
+			rankName = "mvpPlus";
+			break;
+		case "[MVP++]":
+			rankName = "mvpPlusPlus";
+			break;
+		case null:
+			return "";
+		default:
+			return rank;
 	}
 
 	// TODO: processenv variable for activating emoji ranks
