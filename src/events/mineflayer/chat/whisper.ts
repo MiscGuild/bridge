f7d061dbea741eb60832bf0a5de358ba1d26410b
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-10 16:50:59 +1000

    refactor: remove event from master

--- src/events/mineflayer/chat/whisper.ts
+++ src/events/mineflayer/chat/whisper.ts
@@ -1,5 +1,4 @@
 import { Event } from "../../../interfaces/Event";
-import { TextChannel } from "discord.js";
 import fetchHypixelGuild from "../../../util/requests/fetchHypixelGuild";
 import fetchMojangProfile from "../../../util/requests/fetchMojangProfile";
 import isFetchError from "../../../util/requests/isFetchError";
@@ -8,12 +7,6 @@ export default {
 	name: "chat:whisper",
 	runOnce: false,
 	run: async (bot, playerName: string, message: string) => {
-		if (message.toLowerCase().startsWith("register")) {
-			await (bot.discord.channels.cache.get("995070669991723109") as TextChannel).send(playerName);
-			bot.executeCommand(`/w ${playerName} You have been signed up for the event!`);
-			return;
-		}
-
 		const errorMessage = `/w ${playerName} There was an error attempting your request! (Check spelling and/or try again later)`;
 		const target = message.startsWith("weeklygexp" || "weeklygxp") ? playerName : (message.split(" ")[0] as string);
 		const mojangProfile = await fetchMojangProfile(target);
