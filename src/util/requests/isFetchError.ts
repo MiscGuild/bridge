1032b235485f2099c64b9117bee0ab0c22a698f1
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-14 21:16:19 +1000

    fix: prevent errors when searching falsey response in isFetchError

--- src/util/requests/isFetchError.ts
+++ src/util/requests/isFetchError.ts
@@ -2,5 +2,5 @@ import { FetchError } from "../../interfaces/FetchError";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default (response: any | FetchError): response is FetchError => {
-	return "status" && "statusText" in response;
+	return !!response && "status" in response && "statusText" in response;
 };
