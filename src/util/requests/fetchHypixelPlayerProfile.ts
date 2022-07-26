a322181f5ef9cdab2d55e7a1ed24c5f859a9d0dd
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-07-26 22:23:23 +1000

    fix: resolve errors in fetch functions

--- src/util/requests/fetchHypixelPlayerProfile.ts
+++ src/util/requests/fetchHypixelPlayerProfile.ts
@@ -6,6 +6,6 @@ export default async (username: string) => {
 	const response = await fetch(`https://api.hypixel.net/player?key=${process.env.HYPIXEL_API_KEY}&name=${username}`);
 
 	return response.status === 200
-		? ((await response.json()).player as HypixelPlayerResponse)
+		? (((await response.json()) as any).player as HypixelPlayerResponse) // eslint-disable-line @typescript-eslint/no-explicit-any
 		: (response as FetchError);
 };
