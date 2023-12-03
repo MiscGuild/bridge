b0ed71fcfccf4c6df0a40497353effd82adf9413
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2023-12-03 23:21:54 +1100

    fix: remove node-fetch for v18 fetch API

--- src/util/requests/fetchHypixelGuild.ts
+++ src/util/requests/fetchHypixelGuild.ts
@@ -1,6 +1,5 @@
 import { FetchError } from "../../interfaces/FetchError";
 import { HypixelGuildResponse } from "../../interfaces/HypixelGuildReponse";
-import fetch from "node-fetch";
 
 export default async (uuid: string) => {
 	const response = await fetch(`https://api.hypixel.net/guild?key=${process.env.HYPIXEL_API_KEY}&player=${uuid}`);
--- src/util/requests/fetchHypixelPlayerProfile.ts
+++ src/util/requests/fetchHypixelPlayerProfile.ts
@@ -1,6 +1,5 @@
 import { FetchError } from "../../interfaces/FetchError";
 import { HypixelPlayerResponse } from "../../interfaces/HypixelPlayerResponse";
-import fetch from "node-fetch";
 
 export default async (username: string) => {
 	const response = await fetch(`https://api.hypixel.net/player?key=${process.env.HYPIXEL_API_KEY}&name=${username}`);
