7eb050f28b988fd9756a10807282e8d374357274
Amogh <axm0754@mavs.uta.edu>
2025-04-17 15:48:31 -0500

    fix: update game mode reference from Bedwars to Duels in stats command

--- src/events/mineflayer/chat/fetch-stats/gamemodes/duels/duels-stats-bridge.ts
+++ src/events/mineflayer/chat/fetch-stats/gamemodes/duels/duels-stats-bridge.ts
@@ -37,7 +37,7 @@ export default {
     name: 'chat:duels-bridge',
     runOnce: false,
     run: async (bot, channel, playerRank, playerName, guildRank, target) => {
-        await handleStatsCommand(bot, channel, playerRank, playerName, guildRank, target, 'Bedwars', buildStatsMessage);
+        await handleStatsCommand(bot, channel, playerRank, playerName, guildRank, target, 'Duels', buildStatsMessage);
     }
 } as Event;
 
