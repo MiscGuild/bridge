228eed512d331e7f8cfc7e0383168a62fc7da261
Amogh <axm0754@mavs.uta.edu>
2025-05-06 02:47:35 -0500

    fix: remove unnecessary blank line in handleStatsCommand function

--- src/events/mineflayer/chat/fetch-stats/utils/handleStatsCommand.ts
+++ src/events/mineflayer/chat/fetch-stats/utils/handleStatsCommand.ts
@@ -40,7 +40,6 @@ export default async function handleStatsCommand(
     } else {
         playerData = await fetchHypixelPlayerProfile(lookupName);
     }
-
     if (isFetchError(playerData)) {
         handleFetchError(playerData, playerName, lookupName, bot);
         return;
