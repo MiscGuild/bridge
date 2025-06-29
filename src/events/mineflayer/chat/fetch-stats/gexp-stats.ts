8802e9c015d961fa2512bf2fe71109dc9fefa801
Vliegenier04 <info@vliegenier04.dev>
2025-06-29 22:56:13 +0200

    Fixed the GEXP stats message to include a new range for 100,000 to 250,000 GEXP

--- src/events/mineflayer/chat/fetch-stats/gexp-stats.ts
+++ src/events/mineflayer/chat/fetch-stats/gexp-stats.ts
@@ -9,17 +9,17 @@ function buildGEXPMessage(
 ): string {
     const playerGEXPAccumulated = playerGEXP.reduce((acc: any, curr: any) => acc + curr, 0);
     if (playerGEXPAccumulated === 0) {
-        return `/${runChannel} ${requestName} No GEXP stats found for ${lookupName}. Make sure the player has been active in the past 7 days. | ${getRandomHexColor()}`;
+        return `/${runChannel} No GEXP stats found for ${lookupName}. Make sure the player has been active in the past 7 days. | ${getRandomHexColor()}`;
     } else if (playerGEXPAccumulated < 5000) {
-        return `/${runChannel} ${requestName} ${lookupName} has a total of ${playerGEXPAccumulated} GEXP accumulated over the past 7 days. Keep grinding! | ${getRandomHexColor()}`;
+        return `/${runChannel} Great! ${lookupName} has a total of ${playerGEXPAccumulated} GEXP accumulated over the past 7 days. Keep grinding! This member is egible for kick by the way ;-; | ${getRandomHexColor()}`;
     } else if (playerGEXPAccumulated < 10000) {
-        return `/${runChannel} ${requestName} Great job, ${lookupName}! You've accumulated ${playerGEXPAccumulated} GEXP over the past week. Keep it up! | ${getRandomHexColor()}`;
+        return `/${runChannel} Wowies! ${lookupName} has accumulated ${playerGEXPAccumulated} GEXP over the past week. Keep it up! This member is egible for kick by the way ;-; | ${getRandomHexColor()}`;
     } else if (playerGEXPAccumulated < 100000) {
-        return `/${runChannel} ${requestName} Impressive, ${lookupName}! You've racked up ${playerGEXPAccumulated} GEXP in the last 7 days. Keep pushing! | ${getRandomHexColor()}`;
-    } else if (playerGEXPAccumulated < 1000000) {
-        return `/${runChannel} ${requestName} Amazing work, ${lookupName}! You've accumulated ${playerGEXPAccumulated} GEXP over the past week. You're on fire! | ${getRandomHexColor()}`;
-    } else if (playerGEXPAccumulated < 250000) {
-        return `/${runChannel} ${requestName} Outstanding, ${lookupName}! You've amassed a whopping ${playerGEXPAccumulated} GEXP in the last 7 days. Keep dominating! | ${getRandomHexColor()}`;
+        return `/${runChannel} Impressive! ${lookupName} has racked up ${playerGEXPAccumulated} GEXP in the last 7 days. Keep pushing! | ${getRandomHexColor()}`;
+    } else if (playerGEXPAccumulated > 100000 && playerGEXPAccumulated < 250000) {
+        return `/${runChannel} Amazing work! ${lookupName} has accumulated ${playerGEXPAccumulated} GEXP over the past week. On fire!| ${getRandomHexColor()}`;
+    } else if (playerGEXPAccumulated > 250000) {
+        return `/${runChannel} Outstandin'! ${lookupName} has amassed a whopping ${playerGEXPAccumulated} GEXP in the last 7 days. Keep dominating! | ${getRandomHexColor()}`;
     } else {
         return `/${runChannel} ${requestName} [GEXP] ${lookupName} has achieved ${playerGEXPAccumulated} GEXP over the past week. | ${getRandomHexColor()}`;
     }
